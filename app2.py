c2bd906a218c235bd796cbb853cd28ee8c66348f
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-05-09 21:58:40 -0400

    added sorting and filtering for addresses

--- app2.py
+++ app2.py
@@ -15,7 +15,7 @@ fields_culled = []
 records_culled = []
 
 # select columns
-required_columns = [28, 29, 30, 32, 33, 34, 43, 44, 73]
+required_columns = [8, 28, 29, 30, 32, 33, 34, 43, 44, 73]
 
 # iterate through fields, selecting only what is required
 for i in range(0,77):
@@ -37,15 +37,84 @@ for rec in records:
             continue
     records_culled.append(row_culled)
 
-         
+# TODO: Split location and mailing addresses 
+# set references on relative index
+loc_columns = [0, 1, 7, 8, 9]
+mail_columns = [0, 1, 2, 3, 4, 5, 6]
 
+# setup fields and records
+loc_fields = []
+mail_fields = []
+loc_records = []
+mail_records = []
+
+# iterate fields
+for i in range(0,10):
+    try:
+        loc_columns.index(i)
+        loc_fields.append(fields_culled[i])
+    except: 
+        continue 
+for i in range(0, 8):
+    try:
+        mail_columns.index(i)
+        mail_fields.append(fields_culled[i])
+    except:
+        continue
+
+# iterate records
+for rec in records_culled:
+    loc_row = []
+    mail_row = []
+    for i in range(0, 10):
+        try:
+            loc_columns.index(i)
+            loc_row.append(rec[i])
+        except:
+            continue
+    for i in range(0, 8):
+        try:
+            mail_columns.index(i)
+            mail_row.append(rec[i])
+        except:
+            continue
+    loc_records.append(loc_row)
+    mail_records.append(mail_row)
+
+# Filter mailing list
+filtered_mail_records = []
+for row in mail_records:
+    if row[0] == row[4]:
+        print(row[0], row[4])
+    else:
+        filtered_mail_records.append(row)    
+
+for filt_rec in filtered_mail_records:
+    print(filt_rec)
 # Create workbook
-wb = Workbook()
-ws = wb.active
+wb1 = Workbook()
+wb2 = Workbook()
+ws1 = wb1.active
+ws2 = wb2.active
 
 # append data to the worksheet
-ws.append(fields_culled)
-for rec in records_culled:
-    ws.append(rec)
+ws1.append(mail_fields)
+for rec in filtered_mail_records:
+    ws1.append(rec)
 
-wb.save('culling_attmpt1.xlsx')
+ws2.append(loc_fields)
+for rec in loc_records:
+    ws2.append(rec)
+
+# # Reorder columns mailing list
+ws1.move_range('F1:F1186', cols=-2)
+ws1.move_range('G1:G1186', cols=-1)
+# Reorder columns locations sheet
+ws2.move_range('B1:B1186', cols=4)
+ws2.move_range('C1:D1186', cols=-1)
+ws2.move_range('F1:F1186', cols=-2)
+
+
+# Save files
+wb1.save('mail_attmpt6shifted.xlsx')
+wb2.save('loc_attmpt6shifted.xlsx')
